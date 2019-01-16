a99cecc2587a354b81df22e521551a6e1769a4fb
Shinnosuke Komiya <kurohune538@gmail.com>
2019-01-16 18:15:25 +0900

    edit profiles

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "White Mage Yamiko", // <title>
+  shortSiteTitle: "White Mage Yamiko", // <title> ending for posts and pages
+  siteDescription: "Yamiko's works and blog.",
+  siteUrl: "https://cham538.com",
   // pathPrefix: "",
   siteImage: "preview.jpg",
-  siteLanguage: "en",
+  siteLanguage: "ja",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "ヤミコ(Yamiko)",
+  authorTwitterAccount: "kurohune538",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "ヤミコ(Yamiko)",
+  headerSubTitle: "White Mage Yamiko",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Yamiko - ヤミコの休息",
+  manifestShortName: "Rest of Yamiko", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/kurohune538" },
+    { name: "twitter", url: "https://twitter.com/kurohune538" },
+    { name: "facebook", url: "https://www.facebook.com/sinnosuke.komiya" }
   ]
 };
--- content/parts/author.md
+++ content/parts/author.md
@@ -2,4 +2,7 @@
 title: author
 ---
 
-**Mr. Gatsby** Proin ornare ligula eu tellus tempus elementum. Aenean [bibendum](/) iaculis mi, nec blandit lacus interdum vitae. Vestibulum non nibh risus, a scelerisque purus. Blandit lacus interdum vitae. Vestibulum non nibh risus, a scelerisque purus.
+**ヤミコ(Yamiko)** 本業は白魔道師。知識の探求と研究が好き。覚えた魔法は人に教えたがる。Art と Science を突き詰めるお仕事をしています。
+Main job is "White mage".
+I love searching knowledge and reserch. I Also like teaching "Magic" what I learned.  
+I work for looking into Art and Science.
